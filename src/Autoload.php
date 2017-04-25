753a152b763ce7a5e0c5305f6a294ae0567892e9
Grzegorz Gąsak <info@bugsmonitor.com>
2017-04-25 23:49:19 +0200

    Update legacy autoloader

--- src/Autoload.php
+++ src/Autoload.php
@@ -3,17 +3,16 @@
  * Autoloader for legacy PHP projects
  */
 
-spl_autoload_register(function ($name) {
+spl_autoload_register(function ($n) {
+    $name = explode('\\', $n);
+    $name = end($name);
     $file = dirname(__FILE__) . '/' . $name . '.php';
-    //if (file_exists($file)) {
-    if (file_exists($name . '.php')) {
-        //require $file;
-        require $name . '.php';
+
+    if (file_exists($file)) {
+        require $file;
 
         return true;
     }
 
     return false;
-    //echo "Want to load $name.\n";
-    //throw new Exception("Unable to load $name.");
 });
